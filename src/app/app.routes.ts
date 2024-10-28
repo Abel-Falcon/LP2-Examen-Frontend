c0509bc7e55eb973d4b0289862dcf5cdbd8549db
Abel-Falon <nehemias.falcon@upeu.edu.pe>
2024-10-28 15:56:02 -0500

    commit en el examen

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { LibroComponent } from './component/libro/libro.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    title: 'Página principal',
+  },
+  {
+    path: 'libro',
+    component: LibroComponent,
+    title: 'Soy Libro',
+  },
+  {
+    path: '**',
+    redirectTo: '',
+    pathMatch: 'full',
+  },
+];
